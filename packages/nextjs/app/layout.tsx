5f016b4ace11dba0fd4e8be08aecfbbd5e53842b
YuChanGongzhu <2556553872@qq.com>
2025-11-14 18:26:44 +0800

    fix -ui

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -6,8 +6,8 @@ import "~~/styles/globals.css";
 import { getMetadata } from "~~/utils/scaffold-eth/getMetadata";
 
 export const metadata = getMetadata({
-  title: "Scaffold-ETH 2 App",
-  description: "Built with 🏗 Scaffold-ETH 2",
+  title: "Zeta Farm S1 DApp",
+  description: "A Farm GameFi on ZetaChain",
 });
 
 const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
